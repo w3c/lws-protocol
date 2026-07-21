ca8dea574d020c00075f59589862f2b0e9260744
renyuneyun (Rui Zhao) <renyuneyun@gmail.com>
2026-07-21 11:41:52 +0800

    reworded Threat 3

--- threat-model/threats/t3-client-id-impersonation.js
+++ threat-model/threats/t3-client-id-impersonation.js
@@ -1,14 +1,14 @@
 (function () {
     var threat = {
         id: "T3",
-        name: "Client ID Impersonation (with Trustworthy User Authorization Server)",
+        name: "Client ID Impersonation (to User Authorization Server)",
         desc: "The Client (Client A) may pretend to be another Client (Client B), to gain authorization it should not have.",
         response: [
             {
                 id: "R4",
                 name: "CID and Environment Verification (from User Authorization Server)",
                 type: "Reduce",
-                desc: "The User Authorization Server verifies the identity of the Client, by checking the environmental information (e.g. redirect URL, Origin) of the Client matches those specified in the Client CID Document.",
+                desc: "The User Authorization Server verifies the identity of the Client, by checking the environmental information (e.g. redirect URL) of the Client matches those specified in the Client CID Document. This ensures that the response that the User Authorization Server sends is to a location trusted by the claimed Client CID Document. As long as those specified locations are not compromised, there will not be impersonations.",
             },
         ],
         elements: ["P3", "P2"],
